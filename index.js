1b8b843090a988413b7a69a0c11673b1f99319eb
Kauã Cavalcante <kauacavalcante777@gmail.com>
2022-05-18 16:56:55 -0300

    feat: add route to get a task by id

--- index.js
+++ index.js
@@ -18,6 +18,21 @@ app.get("/tasks", async (req, res) => {
     res.status(500).send(error.message);
 });
 
+app.get("/tasks/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+
+        const task = await TaskModel.findById(taskId);
+
+        if (!task) {
+            return res.status(404).send("Essa tarefa não foi encontrada.");
+        }
+        return res.status(200).send(task);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
 app.post("/tasks", async (req, res) => {
     try {
         const newTask = new TaskModel(req.body);
@@ -25,6 +40,24 @@ app.post("/tasks", async (req, res) => {
         await newTask.save();
 
         res.status(201).send(newTask);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+
+        const taskToDelete = await TaskModel.findById(taskId);
+
+        if (!taskToDelete) {
+            return res.status(404).send("Essa tarefa não foi encontrada.");
+        }
+
+        const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+
+        res.status(200).send(deletedTask);
     } catch (error) {
         res.status(500).send(error.message);
     }

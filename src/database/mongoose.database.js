09b5a7aefb6aed80b1d0c9790246b55ba7a6d037
Kauã Cavalcante <kauacavalcante777@gmail.com>
2022-05-18 15:29:04 -0300

    feat: connect to mongodb database

---
+++ src/database/mongoose.database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const connectToDatabase = async () => {
+    await mongoose.connect(
+        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fsctaskmanagercluster.372onyw.mongodb.net/?retryWrites=true&w=majority`,
+        (error) => {
+            if (error) {
+                return console.log(
+                    `Could not connect to MongoDB: ${error.message}`
+                );
+            }
+
+            return console.log("Connected to MongoDB!");
+        }
+    );
+};
+
+module.exports = connectToDatabase;

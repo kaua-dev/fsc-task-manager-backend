1151d18e5240be8d1477599f43e591067d1cd70e
Kauã Cavalcante <kauacavalcante777@gmail.com>
2023-06-14 10:51:36 -0300

    feat: listening to enviroment port

--- index.js
+++ index.js
@@ -15,4 +15,6 @@ connectToDatabase();
 
 app.use("/tasks", TaskRouter);
 
-app.listen(8080, () => console.log("Listening on port 8080!"));
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
